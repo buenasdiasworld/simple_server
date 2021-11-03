252ac1d0478ba7cb477fb5f8961ad9a7583ebe65
buenasdiasworld <vittta.litvinova@mail.ru>
2021-11-03 18:17:21 +0300

    commit1

--- src/main/java/main/RebaseController.java
+++ src/main/java/main/RebaseController.java
@@ -4,12 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TaskController {
+public class RebaseController {
 
 
   @GetMapping("/hello")
   public String hello() {
-    return "hello world from back end";
+    return "rebase your branch";
   }
 
 
